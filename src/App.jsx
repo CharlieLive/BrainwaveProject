da8b66236ab833925f8879a6a5c47832b7b5a1b9
CharlieLive <karol.kozaczuk@gmail.com>
2024-04-23 19:20:30 +0200

    Benefits fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
